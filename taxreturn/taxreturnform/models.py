af0d296b0545dba47b3646e1fbee00b324dccf4d
cosmologist10 <sumanshweta44@gmail.com>
2018-06-26 11:06:06 +0530

    Bug of model choice field solved and app added in settings file

--- taxreturn/taxreturnform/models.py
+++ taxreturn/taxreturnform/models.py
@@ -3,49 +3,46 @@ import datetime
 
 # Create your models here.
 
-def year_choices():
-    return [r for r in range(2011, datetime.date.today().year)]
-
-def current_year():
-    return datetime.date.today().year
-
 class TaxProperties(models.Model):
     """ Dashboard Components. """
 
+    YEAR_CHOICES = []
+    for r in range(2011, (datetime.datetime.now().year+1)):
+        YEAR_CHOICES.append(r)
+
     TERM_CHOICES = (
-            (MONTHLY, 'Monthly'),
-            (QUATERLY, 'Quaterly'),
-            (ANNUALLY, 'Annually'),
+            ('MONTHLY', 'Monthly'),
+            ('QUATERLY', 'Quaterly'),
+            ('ANNUALLY', 'Annually'),
             )
 
     TYPE_CHOICES = (
-            (DIRECT, 'Direct'),
-            (INDIRECT, 'Indirect'),
-            (OTHER, 'Other'),
+            ('DIRECT', 'Direct'),
+            ('INDIRECT', 'Indirect'),
+            ('OTHER', 'Other'),
             )
 
     SUBTYPE_CHOICES = (
-            (GSTR1, 'GSTR1'),
-            (GSTR2, 'GSTR2'),
-            (ADVANCETAX, 'Advace Tax'),
-            (TDSRETURN, 'TDS Return'),
-            (CORPORATETAX, 'Corporate Tax'),
-            (GSTR8, 'GSTR8'),
-            (AOC, 'AOC'),
-            (MGT7, 'MGT7'),
-            (AGM, 'AGM'),
-            (TDSCHALLENGE, 'TDS Challenge'),
-            (GSTR3B, 'GSTR 3B'),
-            (CTR, 'CTR'),
-            (GM, 'GM'),
-            (GSTR9, 'GSTR9'),
+            ('GSTR1', 'GSTR1'),
+            ('GSTR2', 'GSTR2'),
+            ('ADVANCETAX', 'Advace Tax'),
+            ('TDSRETURN', 'TDS Return'),
+            ('CORPORATETAX', 'Corporate Tax'),
+            ('GSTR8', 'GSTR8'),
+            ('AOC', 'AOC'),
+            ('MGT7', 'MGT7'),
+            ('AGM', 'AGM'),
+            ('TDSCHALLENGE', 'TDS Challenge'),
+            ('GSTR3B', 'GSTR 3B'),
+            ('CTR', 'CTR'),
+            ('GM', 'GM'),
+            ('GSTR9', 'GSTR9'),
             )
 
 
-    assessment_year = models.IntegerField(choices = year_choices,default=current_year)
-    term = models.CharField(max_length = 8,choices = TERM_CHOICES,default=MONTHLY)
-    types= models.CharField(max_length = 8, choices = TYPE_CHOICES,
-            default=DIRECT)
+    year = models.IntegerField(max_length=4,choices=YEAR_CHOICES,default=datetime.datetime.now().year)
+    term = models.CharField(max_length = 8,choices=TERM_CHOICES,default='MONTHLY')
+    types= models.CharField(max_length = 8, choices = TYPE_CHOICES,default='DIRECT')
     subtype =  models.CharField(max_length = 12,choices = SUBTYPE_CHOICES, null=True)
     files = models.FileField(upload_to ='documents/')
     remarks = models.CharField(max_length=100, blank=True, null = True)
